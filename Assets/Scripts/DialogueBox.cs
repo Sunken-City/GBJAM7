6b78f9c9dc7cc5f517fbaf8717c8ccefa73201d2
Anthony Cloudy <acloudy@mail.smu.edu>
2019-08-18 14:42:54 -0700

    Added a dialogue component!

---
+++ Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueBox : MonoBehaviour
+{
+
+    private Text _dialogueText = null;
+    private Text _nameText = null;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _nameText = transform.Find("NameBox").Find("NameText").GetComponent<Text>();
+        _dialogueText = transform.Find("TextBox").Find("DialogueText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetName(string name)
+    {
+        _nameText.text = name;
+    }
+
+    public void SetDialogue(string text)
+    {
+        _dialogueText.text = text;
+    }
+}
---
+++ Assets/Scripts/DialogueComponent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueComponent : MonoBehaviour
+{
+    public string characterName;
+    public string text;
+    public float interactRadius = 0.2f;
+    public GameObject dialogueBoxPrefab;
+    private GameObject _dialogueBoxInstance = null;
+    private GameObject _playerReference = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerReference = GameObject.FindGameObjectWithTag("Player");
+        Debug.Assert(_playerReference, "Enemies in the scene need to have the player tagged as 'Player'");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!_dialogueBoxInstance)
+        {
+            if(Vector2.Distance(_playerReference.transform.position, transform.position) < interactRadius && Input.GetKeyUp(KeyCode.J))
+            {
+                _dialogueBoxInstance = Instantiate(dialogueBoxPrefab);
+                _dialogueBoxInstance.GetComponent<Canvas>().worldCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+                DialogueBox dialogue = _dialogueBoxInstance.GetComponent<DialogueBox>();
+                dialogue.SetName(characterName);
+                dialogue.SetDialogue(text);
+                OverworldController.instance.freezeInput = true;
+            }
+        }
+        else if(Input.GetKeyUp(KeyCode.J) || Input.GetKeyUp(KeyCode.K))
+        {
+            Destroy(_dialogueBoxInstance);
+            _dialogueBoxInstance = null;
+            OverworldController.instance.freezeInput = false;
+        }
+    }
+}
