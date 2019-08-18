afc8f16c097776cf58f6d5be2f482f5618903953
Jonathan Lyttle <myncijub@gmail.com>
2019-08-17 20:05:11 -0500

    Added overworld player walk cycle animation (#1)
    * Added test player prefab
    
    * Added test walking and idle animations
    
    * Fixed issue with uninterruptable animations
    
    * Fix broken idle animations
    
    * Moved test player to Player folder

--- Assets/Scripts/Overworld/OverworldPlayerController.cs
+++ Assets/Scripts/Overworld/OverworldPlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class OverworldPlayerController : MonoBehaviour
 {
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,27 +18,58 @@ public class OverworldPlayerController : MonoBehaviour
         UpdateInput();
     }
 
+    void FixedUpdate()
+    {
+        UpdateAnimation();
+    }
+
+    void UpdateAnimation()
+    {
+    }
+
     void UpdateInput() 
     {
+        bool isWalking = false;
         if(OverworldController.instance.freezeInput)
         {
+            animator.SetBool("walking", false);
             return;
         }
         if(Input.GetKey(KeyCode.A))
         {
+            isWalking = true;
             transform.position += new Vector3(-0.01f, 0, 0);
+            animator.SetFloat("x", -1f);
+            animator.SetFloat("y", 0f);
         }
-        if(Input.GetKey(KeyCode.D))
+        else if(Input.GetKey(KeyCode.D))
         {
+            isWalking = true;
             transform.position += new Vector3(0.01f, 0, 0);
+            animator.SetFloat("x", 1f);
+            animator.SetFloat("y", 0f);
         }
         if(Input.GetKey(KeyCode.W))
         {
+            isWalking = true;
             transform.position += new Vector3(0, 0.01f, 0);
+            animator.SetFloat("x", 0f);
+            animator.SetFloat("y", 1f);
         }
-        if(Input.GetKey(KeyCode.S))
+        else if(Input.GetKey(KeyCode.S))
         {
+            isWalking = true;
             transform.position += new Vector3(0, -0.01f, 0);
+            animator.SetFloat("x", 0f);
+            animator.SetFloat("y", -1f);
+        }
+        if(isWalking)
+        {
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
         }
     }
 }
