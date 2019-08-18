a1905cfe455d50dff63319b9aa3aead3a551e8b6
weruder <myncijub@gmail.com>
2019-08-17 23:46:50 -0500

    Added lose on input controller, delta option for shake

---
+++ Assets/ButtonObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonObject : MonoBehaviour
+{
+    public Sprite alternateState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<SpriteRenderer>().sprite = alternateState;
+        MicrogameController.instance.LoseMicrogame();
+    }
+}
---
+++ Assets/Scripts/Microgame Player Controllers/LoseOnInputController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseOnInputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("W") || Input.GetKey("A") || Input.GetKey("S") || Input.GetKey("D") || Input.GetKey("J") || Input.GetKey("K"))
+        {
+            MicrogameController.instance.LoseMicrogame();
+        }
+    }
+}
