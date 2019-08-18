6b1f9e6f17a067478e2f8077bbfc82c149588940
Anthony Cloudy <acloudy@mail.smu.edu>
2019-08-17 19:49:23 -0700

    Frog date (#3)
    * Safety commit for our baby boy
    
    * Picking up Q's work on the new import settings <3
    
    * IT'S WORKING AAAAAA

---
+++ Assets/FrogDateGameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogDateGameController : MonoBehaviour
+{
+    public GameObject dateCardPrefab;
+    public GameObject loseObject;
+    public GameObject winObject;
+    public int numCards = 4;
+    private int _currentCardIndex = 0;
+
+    private GameObject[] _cards;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cards = new GameObject[numCards];
+        _currentCardIndex = numCards - 1;
+
+        for(int i = 0; i < numCards; ++i)
+        {
+            _cards[i] = Instantiate(dateCardPrefab, this.transform.position, this.transform.rotation, this.transform.parent);
+        }
+
+        _cards[Random.Range(0, numCards)].GetComponent<DateCard>().MakeOurBoy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MicrogameController.instance.HasWon())
+        {
+            winObject.GetComponent<SpriteRenderer>().enabled = true;
+            return;
+        }
+        else if(MicrogameController.instance.HasLost())
+        {
+            loseObject.GetComponent<SpriteRenderer>().enabled = true;
+            return;
+        }
+
+        if(Input.GetKeyUp(KeyCode.A))
+        {
+            _cards[_currentCardIndex--].GetComponent<DateCard>().SwipeLeft();
+        }
+        else if(Input.GetKeyUp(KeyCode.D))
+        {
+            _cards[_currentCardIndex--].GetComponent<DateCard>().SwipeRight();
+        }
+    }
+}
