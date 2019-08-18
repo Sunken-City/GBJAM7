14fe1781faf38aba0d9332e96a48e69cb1c2eecc
weruder <myncijub@gmail.com>
2019-08-17 19:02:56 -0500

    Added test walking and idle animations

--- Assets/Scripts/Overworld/OverworldPlayerController.cs
+++ Assets/Scripts/Overworld/OverworldPlayerController.cs
@@ -4,16 +4,63 @@ using UnityEngine;
 
 public class OverworldPlayerController : MonoBehaviour
 {
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdateInput();
+    }
+
+    void FixedUpdate()
+    {
+        UpdateAnimation();
+    }
+
+    void UpdateAnimation()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        if(xAxis != 0 || yAxis != 0)
+        {
+            animator.SetBool("walking", true);
+
+            if (xAxis == 0 && yAxis > 0)
+            {
+                //Player is facing up
+                animator.SetFloat("x", 0f);
+                animator.SetFloat("y", 1f);
+            }
+            else if (xAxis == 0 && yAxis < 0)
+            {
+                //Player is facing down
+                animator.SetFloat("x", 0f);
+                animator.SetFloat("y", -1f);
+            }
+            else if (xAxis > 0 && yAxis == 0)
+            {
+                //Player is facing right
+                animator.SetFloat("x", 1f);
+                animator.SetFloat("y", 0f);
+            }
+            else
+            {
+                //Player is facing left
+                animator.SetFloat("x", -1f);
+                animator.SetFloat("y", 0f);
+            }
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+        }
     }
 
     void UpdateInput() 
