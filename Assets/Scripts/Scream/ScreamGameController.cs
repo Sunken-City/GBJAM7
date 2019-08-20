3b2042412277abf7f64cf753dead8983ab379744
Anthony Cloudy <acloudy@mail.smu.edu>
2019-08-19 22:14:54 -0700

    Added the scream microgame.

---
+++ Assets/Scripts/Scream/ScreamGameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreamGameController : MonoBehaviour
+{
+    public Sprite screamSprite;
+    public CanvasGroup smallScream;
+    public CanvasGroup loudScream;
+
+    private InflateController _inflateControllerRef;
+    private ViolentShakingComponent _violentShakeRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _inflateControllerRef = GetComponent<InflateController>();
+        _violentShakeRef = GetComponent<ViolentShakingComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _violentShakeRef.shakeMagnitude = (_inflateControllerRef.Scale - 1.0f) / 2.0f;
+        if(_violentShakeRef.shakeMagnitude < 0.1f)
+        {
+            _violentShakeRef.shakeMagnitude = 0.0f;
+        }
+
+        if(!_inflateControllerRef.CanContinueToInflate)
+        {
+            GetComponent<SpriteRenderer>().sprite = screamSprite;
+        }
+
+        if(MicrogameController.instance.HasWon())
+        {
+            loudScream.alpha = 1.0f;
+        }
+        if(MicrogameController.instance.HasLost())
+        {
+            smallScream.alpha = 1.0f;
+        }
+    }
+}
