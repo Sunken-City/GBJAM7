43c837c665695d0042ebff0419ed94709851479a
soxymo <soxymo@aol.com>
2019-08-20 20:20:11 -0700

    Smooch game setup (#10)
    * Added Smooch Microgame
    
    * Fixed broken smooch game
    
    * Picked out a color pallette for Smooch

---
+++ Assets/Scripts/Smooch/SmoochHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoochHandler : MonoBehaviour
+{
+
+    public GameObject lips;
+    public GameObject heart;
+
+    private InflateController _inflateController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lips.SetActive(false);
+        heart.SetActive(false);
+        _inflateController = GetComponent<InflateController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MicrogameController.instance.HasWon())
+        {
+            heart.SetActive(true);
+            lips.SetActive(true);
+            
+        }
+    }
+}
