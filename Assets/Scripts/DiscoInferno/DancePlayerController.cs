00ea3e772cda553939158c86f4acae1ad036f4a6
Anthony Cloudy <acloudy@mail.smu.edu>
2019-08-20 19:50:17 -0700

    Disco Inferno is complete!

--- Assets/Scripts/DiscoInferno/DancePlayerController.cs
+++ Assets/Scripts/DiscoInferno/DancePlayerController.cs
@@ -6,9 +6,11 @@ public class DancePlayerController : MonoBehaviour
 {
     public bool freezeOnVictory = false;
     public bool freezeOnLoss = false;
-
     public Sprite[] danceSprites;
     public AudioClip[] danceSounds;
+
+    private int _numDanceMoves = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,30 +33,40 @@ public class DancePlayerController : MonoBehaviour
             renderer.sprite = danceSprites[1];
             bounce.bounceDirection = new Vector2(-0.1f, 0.0f);
             audio.PlayOneShot(danceSounds[0]);
+            ++_numDanceMoves;
         }
         else if(Input.GetKeyDown(KeyCode.D))
         {
             renderer.sprite = danceSprites[2];  
             bounce.bounceDirection = new Vector2(0.1f, 0.0f);     
-            audio.PlayOneShot(danceSounds[1]);     
+            audio.PlayOneShot(danceSounds[1]); 
+            ++_numDanceMoves;    
         }
         else if(Input.GetKeyDown(KeyCode.W))
         {
             renderer.sprite = danceSprites[3];   
             bounce.bounceDirection = new Vector2(0.0f, 0.1f);      
             audio.PlayOneShot(danceSounds[2]);   
+            ++_numDanceMoves;
         }
         else if(Input.GetKeyDown(KeyCode.S))
         {
             renderer.sprite = danceSprites[4];           
             bounce.bounceDirection = new Vector2(0.0f, -0.1f);   
             audio.PlayOneShot(danceSounds[3]);  
+            ++_numDanceMoves;
         }
         else if(Input.GetKeyDown(KeyCode.J))
         {
             renderer.sprite = danceSprites[5];  
             bounce.bounceDirection = new Vector2(0.0f, 0.0f);     
-            audio.PlayOneShot(danceSounds[4]);         
+            audio.PlayOneShot(danceSounds[4]);    
+            ++_numDanceMoves;     
+        }
+
+        if(_numDanceMoves > 15 && MicrogameController.instance.HasNotYetWon())
+        {
+            MicrogameController.instance.WinMicrogame();
         }
     }
 }
