72d56d3640f3a5725258fee1c1d19aba7194af8d
Anthony Cloudy <acloudy@mail.smu.edu>
2019-08-20 19:20:22 -0700

    Disco Inferno progress!

---
+++ Assets/Scripts/DiscoInferno/DancePlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DancePlayerController : MonoBehaviour
+{
+    public bool freezeOnVictory = false;
+    public bool freezeOnLoss = false;
+
+    public Sprite[] danceSprites;
+    public AudioClip[] danceSounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((freezeOnVictory && MicrogameController.instance.HasWon()) || (freezeOnLoss && MicrogameController.instance.HasLost()))
+        {
+            return;
+        }
+        
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        DirectionBounce bounce = GetComponent<DirectionBounce>();
+        AudioSource audio = GetComponent<AudioSource>();
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            renderer.sprite = danceSprites[1];
+            bounce.bounceDirection = new Vector2(-0.1f, 0.0f);
+            audio.PlayOneShot(danceSounds[0]);
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            renderer.sprite = danceSprites[2];  
+            bounce.bounceDirection = new Vector2(0.1f, 0.0f);     
+            audio.PlayOneShot(danceSounds[1]);     
+        }
+        else if(Input.GetKeyDown(KeyCode.W))
+        {
+            renderer.sprite = danceSprites[3];   
+            bounce.bounceDirection = new Vector2(0.0f, 0.1f);      
+            audio.PlayOneShot(danceSounds[2]);   
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            renderer.sprite = danceSprites[4];           
+            bounce.bounceDirection = new Vector2(0.0f, -0.1f);   
+            audio.PlayOneShot(danceSounds[3]);  
+        }
+        else if(Input.GetKeyDown(KeyCode.J))
+        {
+            renderer.sprite = danceSprites[5];  
+            bounce.bounceDirection = new Vector2(0.0f, 0.0f);     
+            audio.PlayOneShot(danceSounds[4]);         
+        }
+    }
+}
