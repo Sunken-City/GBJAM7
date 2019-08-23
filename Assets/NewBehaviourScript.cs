1deff4e273b0f48d989b44b6b6699cd715ca30ec
ozabell <kidlatmc@gmail.com>
2019-08-22 21:56:04 -0700

    added another slime to start scripting

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tapping : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    private bool _allTapped = false;
-
     // Start is called before the first frame update
     void Start()
     {
