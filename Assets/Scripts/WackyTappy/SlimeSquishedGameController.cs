aaf512552fdad723bf93e5917bc8a2c8d5fd224d
ozabell <kidlatmc@gmail.com>
2019-08-24 21:58:42 -0700

    copied some methods from the orignal microgame controller trying to figure out what to use

--- Assets/Scripts/WackyTappy/SlimeSquishedGameController.cs
+++ Assets/Scripts/WackyTappy/SlimeSquishedGameController.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class SlimeSquishedGameController : MonoBehaviour
 {
-    public static int numOfSquishedSlimes;
 
+    public enum State
+    {
+        NOT_STARTED = 0,
+        PLAYING,
+        WON,
+        LOST,
+        NUM_STATES
+    }
+    public static int numOfSquishedSlimes;
+    private State _microgameState = State.NOT_STARTED;
+    public float timeLimitSeconds = 5.0f;
+    private float _currentTimerSeconds = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +26,62 @@ public class SlimeSquishedGameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _currentTimerSeconds += Time.deltaTime;
+        //This is a hack to prevent debug spew. If you're debugging an individual scene, you won't have an Overworld Controller.
+        if (OverworldController.instance && _currentTimerSeconds > timeLimitSeconds)
+        {
+            ReturnToOverworld();
+        }
+    }
+
+    public void ReturnToOverworld()
+    {
+        if ((HasNotYetWon() && !winOnTimeOut) || HasLost())
+        {
+            OverworldController.instance.lastMicrogameState = State.LOST;
+        }
+        else
+        {
+            OverworldController.instance.lastMicrogameState = State.WON;
+        }
+
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(OverworldController.instance.currentSceneName));
+        OverworldController.instance.EndMicrogame();
+        Destroy(microgameSubScene);
+    }
+    public void WinMicrogame()
+    {
+        if (_microgameState == State.LOST)
+        {
+            Debug.Log("We've already lost, so we can't win.");
+            return;
+        }
+        _microgameState = State.WON;
+        Debug.Log("Large winner!!!");
+    }
+
+    public void LoseMicrogame()
+    {
+        if (_microgameState == State.WON)
+        {
+            Debug.Log("We've already won, so we can't lose."); //Or can we? To be revisited in case a microgame needs it.
+            return;
+        }
+        _microgameState = State.LOST;
+        Debug.Log("Big loser!!!");
+    }
+
+    public bool HasWon()
+    {
+        return _microgameState == State.WON;
+    }
+    public bool HasLost()
+    {
+        return _microgameState == State.LOST;
+    }
+
+    public bool HasNotYetWon()
+    {
+        return (_microgameState == State.PLAYING || _microgameState == State.NOT_STARTED);
     }
 }
