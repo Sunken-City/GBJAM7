1068d0aa8d147dcc5597ddb4ad88eb8f674a7978
weruder <myncijub@gmail.com>
2019-08-25 14:46:22 -0500

    Added credits scene

---
+++ Assets/Scripts/TitleScreen/CreditsScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsScript : MonoBehaviour
+{
+    public TextAsset creditsFile;
+    //private string _credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_credits = creditsFile.text;
+        gameObject.GetComponent<Text>().text = creditsFile.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0.005f, 0);
+    }
+}
