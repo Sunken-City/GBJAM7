2a5fef51f478d04232bdf4c46194008ddac5784d
Jonathan Lyttle <myncijub@gmail.com>
2019-08-25 16:11:19 -0500

    Added credits scene (#22)
    * Added credits scene
    
    * Speed up text, load title when finished
    
    * Added selector to title screen
    
    * Changed credits font sizer

---
+++ Assets/Scripts/TitleScreen/CreditsScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditsScript : MonoBehaviour
+{
+    public TextAsset creditsFile;
+    public Camera camera;
+    private Text _textComponent = null;
+    private Vector2 _relativePosition;
+    //private string _credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _textComponent = gameObject.GetComponent<Text>();
+        _textComponent.text = creditsFile.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.J))
+        {
+            transform.position += new Vector3(0, 0.02f, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(0, 0.005f, 0);
+        }
+        _relativePosition = camera.transform.InverseTransformDirection(transform.position - camera.transform.position);
+        if(Input.GetKey(KeyCode.K) || _relativePosition.y > (_textComponent.preferredHeight / 100))
+        {
+            SceneManager.LoadScene("TitleScreen");
+        }
+    }
+}
---
+++ Assets/Scripts/TitleScreen/TitleSelectorComponent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSelectorComponent : MonoBehaviour
+{
+    public int numOptions = 0;
+    public int _curOption = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S) && _curOption < numOptions - 1)
+        {
+            ++_curOption;
+            gameObject.GetComponent<SpriteRenderer>().transform.position += new Vector3(0, -0.2f, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.W) && _curOption > 0)
+        {
+            --_curOption;
+            gameObject.GetComponent<SpriteRenderer>().transform.position += new Vector3(0, 0.2f, 0);
+        }
+        if(Input.GetKey(KeyCode.J))
+        {
+            if(_curOption == 0)
+            {
+                SceneManager.LoadScene("Overworld", LoadSceneMode.Single);
+            }
+            else if (_curOption == 1)
+            {
+                SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+            }
+        }
+    }
+}
