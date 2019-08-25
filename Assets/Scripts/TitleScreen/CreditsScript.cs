cf32f690c945b97d9b8cea44659b56eddb8a1df5
weruder <myncijub@gmail.com>
2019-08-25 15:21:56 -0500

    Speed up text, load title when finished

--- Assets/Scripts/TitleScreen/CreditsScript.cs
+++ Assets/Scripts/TitleScreen/CreditsScript.cs
@@ -2,22 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CreditsScript : MonoBehaviour
 {
     public TextAsset creditsFile;
+    public Camera camera;
+    private Text _textComponent = null;
+    private Vector2 _relativePosition;
     //private string _credits;
 
     // Start is called before the first frame update
     void Start()
     {
-        //_credits = creditsFile.text;
-        gameObject.GetComponent<Text>().text = creditsFile.text;
+        _textComponent = gameObject.GetComponent<Text>();
+        _textComponent.text = creditsFile.text;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, 0.005f, 0);
+        if(Input.GetKey(KeyCode.J))
+        {
+            transform.position += new Vector3(0, 0.02f, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(0, 0.005f, 0);
+        }
+        _relativePosition = camera.transform.InverseTransformDirection(transform.position - camera.transform.position);
+        if(_relativePosition.y > (_textComponent.preferredHeight / 100))
+        {
+            SceneManager.LoadScene("TitleScreen");
+            //Destroy(gameObject);
+        }
     }
 }
