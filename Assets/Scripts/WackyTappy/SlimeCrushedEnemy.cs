ec82a1411a3e907218aec37af2215da72c807a3a
ozabell <kidlatmc@gmail.com>
2019-08-24 19:47:00 -0700

    continuing to work on scripting

---
+++ Assets/Scripts/WackyTappy/SlimeCrushedEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeCrushedEnemy : MonoBehaviour
+{
+
+    public static int numOfSquishedSlimes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfSquishedSlimes = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(numOfSquishedSlimes < 5)
+        {
+            if( ) //a slime is squished then add 1 to numOfSwishedSlimes
+            {
+                ++numOfSquishedSlimes; 
+            }
+            //being squished means that the finger circle collider collided with a slime collider
+        }
+
+        //when numOfSquishedSlimes = 5 the game is won
+    }
+}
