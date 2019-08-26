c7a0b56989b300b856796f79fde8e88ac2d12df8
Anthony Cloudy <acloudy@mail.smu.edu>
2019-08-25 17:02:19 -0700

    Removing compression from the assets.

---
+++ Assets/Resolution.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resolution : MonoBehaviour
+{
+    void Awake()
+    {
+        Screen.SetResolution(160, 144, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
